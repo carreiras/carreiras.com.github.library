276f7466581809c84d50caa9a25b2a577427bbf0
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-05-22 10:52:32 -0300

    first commit

---
+++ src/test/java/com/ewecarreira/library/TddLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewecarreira.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
