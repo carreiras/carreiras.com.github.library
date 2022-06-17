681da78d2329fdd83fe29dbecfa54a970159e92c
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-17 13:18:02 -0300

    Configurando o servidor MailTrap para envio de emails

---
+++ src/main/java/com/ewecarreira/library/services/impl/EmailServiceImpl.java
@@ -0,0 +1,15 @@
+package com.ewecarreira.library.services.impl;
+
+import java.util.List;
+
+import com.ewecarreira.library.services.EmailService;
+
+public class EmailServiceImpl implements EmailService{
+
+    @Override
+    public void sendEmails(String message, List<String> emailsList) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
