6866f016dd7d189a1cf96bce87e2d6bdb8666dc7
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-17 13:53:10 -0300

    Comentando o teste de envio de emails

--- src/main/java/com/ewecarreira/library/LibraryApiApplication.java
+++ src/main/java/com/ewecarreira/library/LibraryApiApplication.java
@@ -1,38 +1,30 @@
 package com.ewecarreira.library;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.ewecarreira.library.services.EmailService;
-
-@EnableScheduling
+// @EnableScheduling
 @SpringBootApplication
 public class LibraryApiApplication {
 
-	@Autowired
-	private EmailService emailService;
+	// @Autowired
+	// private EmailService emailService;
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 
-	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-			List<String> emails = Arrays.asList("48b9750bf6-8cac98+1@inbox.mailtrap.io");
-			emailService.sendEmails("Testando o serviço de emails", emails);
-			System.out.println("E-mails enviados!!!!");
-		};
-	}
+	// @Bean
+	// public CommandLineRunner runner() {
+	// 	return args -> {
+	// 		List<String> emails = Arrays.asList("48b9750bf6-8cac98+1@inbox.mailtrap.io");
+	// 		emailService.sendEmails("Testando o serviço de emails", emails);
+	// 		System.out.println("E-mails enviados!!!!");
+	// 	};
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
