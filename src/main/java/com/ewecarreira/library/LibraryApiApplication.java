cd9676d149e2665cd76fa2e90d5a83dac064cbbf
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-17 12:08:13 -0300

    Implementando o serviço de agendamento de tarefas

--- src/main/java/com/ewecarreira/library/LibraryApiApplication.java
+++ src/main/java/com/ewecarreira/library/LibraryApiApplication.java
@@ -4,20 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-@EnableScheduling
 @SpringBootApplication
 public class LibraryApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
-	}
-
-	@Scheduled(cron = "0 54 9 1/1 * ?")
-	public void testAgendamentoTarefas() {
-		System.out.println("AGENDAMENTO DE TAREFAS FUNCIONANDO COM SUCESSO!");
 	}
 
 	@Bean
---
+++ src/main/java/com/ewecarreira/library/services/EmailService.java
@@ -0,0 +1,8 @@
+package com.ewecarreira.library.services;
+
+import java.util.List;
+
+public interface EmailService {
+
+    void sendEmails(String message, List<String> emailsList);
+}
