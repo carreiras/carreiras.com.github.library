c7a3d034c3d88a4a30f66f4163cfa1d8fd813a86
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-17 13:57:50 -0300

    Apagando o teste de envio de emails

--- src/main/java/com/ewecarreira/library/LibraryApiApplication.java
+++ src/main/java/com/ewecarreira/library/LibraryApiApplication.java
@@ -4,27 +4,16 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-// @EnableScheduling
+@EnableScheduling
 @SpringBootApplication
 public class LibraryApiApplication {
-
-	// @Autowired
-	// private EmailService emailService;
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-
-	// @Bean
-	// public CommandLineRunner runner() {
-	// 	return args -> {
-	// 		List<String> emails = Arrays.asList("48b9750bf6-8cac98+1@inbox.mailtrap.io");
-	// 		emailService.sendEmails("Testando o serviço de emails", emails);
-	// 		System.out.println("E-mails enviados!!!!");
-	// 	};
-	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
