831c3e10fffaff09f34c21eb2a5dd92352add3bb
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-23 14:06:23 -0300

    Configurando e executando o Jacoco no projeto

--- src/test/java/com/ewecarreira/library/LibraryApiApplicationTests.java
+++ src/test/java/com/ewecarreira/library/LibraryApiApplicationTests.java
@@ -1,12 +1,13 @@
 package com.ewecarreira.library;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LibraryApiApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Test
+	void contextLoads() {
+	}
 
 }
